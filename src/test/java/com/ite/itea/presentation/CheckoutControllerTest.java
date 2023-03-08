c5d22fb4f38480b85e892956d6c701e51eeddc2d
Sven Hilsmann <sven.hilsmann.ext@uniper.energy>
2023-03-08 11:17:57 +0100

    0013 - Create Integration test for an order with a chair

--- src/test/java/com/ite/itea/presentation/CheckoutControllerTest.java
+++ src/test/java/com/ite/itea/presentation/CheckoutControllerTest.java
@@ -1,9 +1,9 @@
 package com.ite.itea.presentation;
 
-import com.ite.itea.domain.response.ReceiptResponse;
 import com.ite.itea.domain.request.ItemNameRequest;
 import com.ite.itea.domain.request.ItemRequest;
 import com.ite.itea.domain.request.OrderRequest;
+import com.ite.itea.domain.response.ReceiptResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,6 +42,19 @@ class CheckoutControllerTest {
         then(entity.getBody().priceInCents()).isEqualTo(2998L);
         then(entity.getBody().text()).isEqualTo("itea \nPicture 14,99\u00A0€ * 2\nTotal 29,98\u00A0€");
     }
+
+    @Test
+    void shouldReturnCorrectReceiptWhenSendingRequestWithAChairToTheController() {
+        var orderChair = new ItemRequest(ItemNameRequest.Chair, 2);
+        OrderRequest orderRequest = new OrderRequest(List.of((orderChair)));
+
+        var entity = this.testRestTemplate.postForEntity("http://localhost:" + this.port + "/checkout", orderRequest, ReceiptResponse.class);
+
+        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(entity.getBody().priceInCents()).isEqualTo(9998L);
+        then(entity.getBody().text()).isEqualTo("itea \nChair 49,99\u00A0€ * 2\nTotal 99,98\u00A0€");
+    }
+
 
     @Test
     void shouldReturn200WhenSendingRequestToManagementEndpoint() {
