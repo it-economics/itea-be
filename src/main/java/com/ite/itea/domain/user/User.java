b47c3da1baf263df837d028ac1693f7ccbf54479
Raimund Krämer <kraemer_raimund@web.de>
2023-07-01 14:53:58 +0200

    Replace usages of UserDTO with User

--- src/main/java/com/ite/itea/domain/user/User.java
+++ src/main/java/com/ite/itea/domain/user/User.java
@@ -3,4 +3,8 @@ package com.ite.itea.domain.user;
 import java.util.List;
 
 public record User(String firstname, String lastname, List<String> purchasedItems) {
+
+    public String formatFullName() {
+        return firstname() + " " + lastname();
+    }
 }
