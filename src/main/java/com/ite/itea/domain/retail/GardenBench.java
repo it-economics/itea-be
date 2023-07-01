2137e90fa9dcbc4263f37ae29e4852c9cac4315c
Raimund Krämer <kraemer_raimund@web.de>
2023-07-01 18:31:57 +0200

    Refactor formatting of GardenBench description

--- src/main/java/com/ite/itea/domain/retail/GardenBench.java
+++ src/main/java/com/ite/itea/domain/retail/GardenBench.java
@@ -8,11 +8,6 @@ public class GardenBench extends Product {
     private static final double PLANT_ELEMENT_PRICE_IN_EUR = 130;
     private static final double WOOD_PLATE_PRICE_IN_EUR = 70;
     private static final double BACKREST_PRICE_IN_EUR = 50;
-    /**
-     * 1 euro extra charge for each added cm (if more than 165 cm)
-     */
-    private static final double LENGTH_PRICE_EXTRA_CHARGE_IN_EUR = 1;
-    private static final int STANDARD_LENGTH_IN_CM = 165;
 
     private final int lengthInCentimeters;
 
@@ -20,8 +15,6 @@ public class GardenBench extends Product {
     private final int amountPlantElements;
     private final boolean hasBackrest;
     private final boolean isDelivery;
-
-    private String productText;
 
     public GardenBench(ProductId id, int lengthInCentimeters, int amountDefaultElements, int amountPlantElements, boolean hasBackrest, boolean isDelivery) {
         super(id, "Garden bench");
@@ -42,33 +35,14 @@ public class GardenBench extends Product {
 
     @Override
     public String description() {
-        if (productText==null || productText.isEmpty()) {
-            final var productPrice = calculateProductPrice();
-            final var deliveryPrice = calculateDeliveryPrice();
-            productText = calculateProductText(productPrice, deliveryPrice, amountPlantElements, lengthInCentimeters, hasBackrest, isDelivery);
-        }
-        return productText;
-    }
+        final var productPrice = calculateProductPrice();
+        final var deliveryPrice = calculateDeliveryPrice();
 
-    private String calculateProductText(double productPrice, double deliveryPrice, int amountPlantElements, int length, boolean hasBackrest, boolean isDelivery) {
-        int totalLength = calculateTotalLength(amountPlantElements, length);
-        String elementsText = createElementsText(amountPlantElements, hasBackrest);
-        String deliveryText = createDeliveryText(isDelivery, deliveryPrice);
-
-        String productText = "Order for a garden bench:\n";
-        productText += elementsText;
-        productText += "Total length: " + totalLength + " cm\n";
-        productText += deliveryText;
-        if (isDelivery) {
-            productText += "Total price (without delivery): " + productPrice + " EUR";
-            productText += "\n";
-            productText += "Total price (including delivery): " + (productPrice + deliveryPrice) + " EUR";
-            productText += "\n";
-        } else {
-            productText += "Total price: " + productPrice + " EUR";
-            productText += "\n";
-        }
-        return productText;
+        return "Order for a garden bench:\n"
+                + formatElementsText(amountPlantElements, hasBackrest)
+                + "Total length: " + totalLength(amountPlantElements, lengthInCentimeters) + " cm\n"
+                + formatDeliveryText(isDelivery, deliveryPrice)
+                + formatDeliveryPriceText(productPrice, deliveryPrice);
     }
 
     private double calculateProductPrice() {
@@ -79,32 +53,6 @@ public class GardenBench extends Product {
                 + extraLengthPrice()
                 + WOOD_PLATE_PRICE_IN_EUR
                 + (hasBackrest ? BACKREST_PRICE_IN_EUR : 0);
-    }
-
-    private double extraLengthPrice() {
-        if (lengthInCentimeters > STANDARD_LENGTH_IN_CM) {
-            return (lengthInCentimeters - STANDARD_LENGTH_IN_CM) * LENGTH_PRICE_EXTRA_CHARGE_IN_EUR;
-        }
-        return 0;
-    }
-
-    private String createElementsText(int amountPlantElements, boolean hasBackrest) {
-        final var amountPlantElementsText = switch (amountPlantElements) {
-            case 1 -> "Elements: 1 of 2 elements is a plant element";
-            case 2 -> "Elements: 2 of 2 elements is a plant element";
-            default -> "Elements: 0 of 2 elements is a plant element";
-        };
-        final var backRestText = hasBackrest
-                ? ", has a backrest\n"
-                : ", has no backrest\n";
-        return amountPlantElementsText + backRestText;
-    }
-
-    private String createDeliveryText(boolean isDelivery, double deliveryPrice) {
-        String deliveryText = isDelivery
-                ? "Delivery Type: Product is delivered "
-                : "Delivery Type: Product is collected ";
-        return deliveryText + "for " + deliveryPrice + " EUR\n";
     }
 
     private int calculateDeliveryPrice() {
@@ -129,7 +77,44 @@ public class GardenBench extends Product {
         return 0;
     }
 
-    private int calculateTotalLength(int amountPlantElements, int length) {
+    private double extraLengthPrice() {
+        final double lengthPriceExtraChargeInEur = 1.0;
+        final int standardLengthInCm = 165;
+        if (lengthInCentimeters > standardLengthInCm) {
+            return (lengthInCentimeters - standardLengthInCm) * lengthPriceExtraChargeInEur;
+        }
+        return 0;
+    }
+
+    private String formatElementsText(int amountPlantElements, boolean hasBackrest) {
+        final var amountPlantElementsText = switch (amountPlantElements) {
+            case 1 -> "Elements: 1 of 2 elements is a plant element";
+            case 2 -> "Elements: 2 of 2 elements is a plant element";
+            default -> "Elements: 0 of 2 elements is a plant element";
+        };
+        final var backRestText = hasBackrest
+                ? ", has a backrest\n"
+                : ", has no backrest\n";
+        return amountPlantElementsText + backRestText;
+    }
+
+    private String formatDeliveryText(boolean isDelivery, double deliveryPrice) {
+        String deliveryText = isDelivery
+                ? "Delivery Type: Product is delivered "
+                : "Delivery Type: Product is collected ";
+        return deliveryText + "for " + deliveryPrice + " EUR\n";
+    }
+
+    private String formatDeliveryPriceText(double productPrice, double deliveryPrice) {
+        if (isDelivery) {
+            return "Total price (without delivery): " + productPrice + " EUR\n"
+                    + "Total price (including delivery): " + (productPrice + deliveryPrice) + " EUR\n";
+        } else {
+            return "Total price: " + productPrice + " EUR\n";
+        }
+    }
+
+    private int totalLength(int amountPlantElements, int length) {
         return switch (amountPlantElements) {
             case 1 -> length + 60;
             case 2 -> length + 108;
