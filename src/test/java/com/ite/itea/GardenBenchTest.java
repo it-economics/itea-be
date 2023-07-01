0812f88fdd2b34bfe56e13f57dc0eaa5baf9b411
Raimund Krämer <kraemer_raimund@web.de>
2023-07-01 15:36:40 +0200

    Refactor GardenBenchTest for cohesion within test cases

--- src/test/java/com/ite/itea/GardenBenchTest.java
+++ src/test/java/com/ite/itea/GardenBenchTest.java
@@ -12,61 +12,70 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GardenBenchTest {
 
-    private static final GardenBench defaultBench = new GardenBench(1, 165, 2, 0, false, false);
-    private static final GardenBench defaultBenchForDelivery = new GardenBench(1, 165, 2, 0, false, true);
-    private static final GardenBench twoDefaultBenches = new GardenBench(2, 165, 2, 0, false, false);
-    private static final GardenBench onePlantElementBench = new GardenBench(1, 165, 1, 1, false, false);
-    private static final GardenBench backRestBench = new GardenBench(1, 165, 2, 0, true, false);
-
-    private static Stream<Arguments> provideGardenBenchConfigurationsAndRespectivePrices() {
+    private static Stream<Arguments> provideGardenBenchConfigurationsWithRespectivePriceAndDescription() {
         return Stream.of(
-                Arguments.of(defaultBench, EuroPrice.ofCents(23000)),
-                Arguments.of(defaultBenchForDelivery, EuroPrice.ofCents(30000)),
-                Arguments.of(twoDefaultBenches, EuroPrice.ofCents(46000)),
-                Arguments.of(onePlantElementBench, EuroPrice.ofCents(28000)),
-                Arguments.of(backRestBench, EuroPrice.ofCents(28000))
-        );
-    }
-
-    private static Stream<Arguments> provideGardenBenchConfigurationsAndRespectiveDescriptions() {
-        return Stream.of(
-                Arguments.of(defaultBench, "Order for a garden bench:\n" +
-                        "Elements: 0 of 2 elements is a plant element, has no backrest\n" +
-                        "Total length: 181 cm\n" +
-                        "Delivery Type: Product is collected for 0.0 EUR\n" +
-                        "Total price (without delivery): 1 * 230.0 EUR = 230.0 EUR\n"),
-                Arguments.of(defaultBenchForDelivery, "Order for a garden bench:\n" +
-                        "Elements: 0 of 2 elements is a plant element, has no backrest\n" +
-                        "Total length: 181 cm\n" +
-                        "Delivery Type: Product is delivered for 70.0 EUR\n" +
-                        "Total price (without delivery): 1 * 230.0 EUR = 230.0 EUR\n"),
-                Arguments.of(twoDefaultBenches, "Order for a garden bench:\n" +
-                        "Elements: 0 of 2 elements is a plant element, has no backrest\n" +
-                        "Total length: 181 cm\n" +
-                        "Delivery Type: Product is collected for 0.0 EUR\n" +
-                        "Total price (without delivery): 2 * 230.0 EUR = 460.0 EUR\n"),
-                Arguments.of(onePlantElementBench, "Order for a garden bench:\n" +
-                        "Elements: 1 of 2 elements is a plant element, has no backrest\n" +
-                        "Total length: 225 cm\n" +
-                        "Delivery Type: Product is collected for 0.0 EUR\n" +
-                        "Total price (without delivery): 1 * 280.0 EUR = 280.0 EUR\n"),
-                Arguments.of(backRestBench, "Order for a garden bench:\n" +
-                        "Elements: 0 of 2 elements is a plant element, has a backrest\n" +
-                        "Total length: 181 cm\n" +
-                        "Delivery Type: Product is collected for 0.0 EUR\n" +
-                        "Total price (without delivery): 1 * 280.0 EUR = 280.0 EUR\n")
+                Arguments.of(
+                        new GardenBench(1, 165, 2, 0, false, false),
+                        EuroPrice.ofCents(23000),
+                        """
+                                Order for a garden bench:
+                                Elements: 0 of 2 elements is a plant element, has no backrest
+                                Total length: 181 cm
+                                Delivery Type: Product is collected for 0.0 EUR
+                                Total price (without delivery): 1 * 230.0 EUR = 230.0 EUR
+                                """
+                ),
+                Arguments.of(
+                        new GardenBench(1, 165, 2, 0, false, true),
+                        EuroPrice.ofCents(30000),
+                        """
+                                Order for a garden bench:
+                                Elements: 0 of 2 elements is a plant element, has no backrest
+                                Total length: 181 cm
+                                Delivery Type: Product is delivered for 70.0 EUR
+                                Total price (without delivery): 1 * 230.0 EUR = 230.0 EUR
+                                """
+                ),
+                Arguments.of(
+                        new GardenBench(2, 165, 2, 0, false, false),
+                        EuroPrice.ofCents(46000),
+                        """
+                                Order for a garden bench:
+                                Elements: 0 of 2 elements is a plant element, has no backrest
+                                Total length: 181 cm
+                                Delivery Type: Product is collected for 0.0 EUR
+                                Total price (without delivery): 2 * 230.0 EUR = 460.0 EUR
+                                """
+                ),
+                Arguments.of(
+                        new GardenBench(1, 165, 1, 1, false, false),
+                        EuroPrice.ofCents(28000),
+                        """
+                                Order for a garden bench:
+                                Elements: 1 of 2 elements is a plant element, has no backrest
+                                Total length: 225 cm
+                                Delivery Type: Product is collected for 0.0 EUR
+                                Total price (without delivery): 1 * 280.0 EUR = 280.0 EUR
+                                """
+                ),
+                Arguments.of(
+                        new GardenBench(1, 165, 2, 0, true, false),
+                        EuroPrice.ofCents(28000),
+                        """
+                                Order for a garden bench:
+                                Elements: 0 of 2 elements is a plant element, has a backrest
+                                Total length: 181 cm
+                                Delivery Type: Product is collected for 0.0 EUR
+                                Total price (without delivery): 1 * 280.0 EUR = 280.0 EUR
+                                """
+                )
         );
     }
 
     @ParameterizedTest
-    @MethodSource("provideGardenBenchConfigurationsAndRespectivePrices")
-    void shouldCalculateCorrectPrice(GardenBench gardenBench, EuroPrice expectedPrice) {
+    @MethodSource("provideGardenBenchConfigurationsWithRespectivePriceAndDescription")
+    void shouldProvideCorrectPriceAndDescription(GardenBench gardenBench, EuroPrice expectedPrice, String description) {
         assertThat(gardenBench.calculateTotalPrice()).isEqualTo(expectedPrice);
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideGardenBenchConfigurationsAndRespectiveDescriptions")
-    void shouldCalculateCorrectPrice(GardenBench gardenBench, String description) {
         assertThat(gardenBench.getProductText()).isEqualTo(description);
     }
 }
