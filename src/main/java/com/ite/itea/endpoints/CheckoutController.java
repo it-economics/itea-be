c111df1b0b237efa4b3e244025ce46e401a0f6aa
Raimund Krämer <kraemer_raimund@web.de>
2023-07-01 11:54:14 +0200

    Simplify creation of order from order request

--- src/main/java/com/ite/itea/endpoints/CheckoutController.java
+++ src/main/java/com/ite/itea/endpoints/CheckoutController.java
@@ -2,18 +2,14 @@ package com.ite.itea.endpoints;
 
 import com.ite.itea.application.dto.*;
 import com.ite.itea.application.usecase.CalculateTotalPriceUseCase;
-import com.ite.itea.domain.retail.ProductName;
-import com.ite.itea.application.dto.ItemRequest;
-import com.ite.itea.application.dto.OrderRequest;
-import com.ite.itea.application.dto.ReceiptResponse;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class CheckoutController {
@@ -25,37 +21,20 @@ public class CheckoutController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ReceiptResponse calculate(@RequestBody OrderRequest orderRequest) {
-        List<ProductDTO> productDTOList = new ArrayList<>();
+        final List<ProductDTO> productsToOrder = orderRequest.itemRequests().stream()
+                .map(itemRequest -> switch (itemRequest.name()) {
+                    case PictureFinland -> new PicturesDTO(itemRequest.name(), itemRequest.amount(), 1499L);
+                    case PictureNorway -> new PicturesDTO(itemRequest.name(), itemRequest.amount(), 999L);
+                    case PictureSweden -> new PicturesDTO(itemRequest.name(), itemRequest.amount(), 1299L);
+                    case ChairElsa -> new ChairsDTO(itemRequest.name(), itemRequest.amount(), 500, 500, 500, "plastic");
+                    case ChairLars -> new ChairsDTO(itemRequest.name(), itemRequest.amount(), 200, 2000, 3000, "metal");
+                    case ChairKnut -> new ChairsDTO(itemRequest.name(), itemRequest.amount(), 400, 1000, 1500, "wood");
+                    case TableLotta -> new TablesDTO(itemRequest.name(), itemRequest.amount(), 1000, 1000, "plastic");
+                    case TableLola -> new TablesDTO(itemRequest.name(), itemRequest.amount(), 3000, 1000, "plastic");
+                })
+                .collect(Collectors.toList());
 
-        for (ItemRequest itemRequest : orderRequest.itemRequests()) {
-            if (ProductName.PictureNorway.equals(itemRequest.name())) {
-                ProductDTO productDTO = new PicturesDTO(itemRequest.name(), itemRequest.amount(), 999L);
-                productDTOList.add(productDTO);
-            } else if (ProductName.PictureSweden.equals(itemRequest.name())) {
-                ProductDTO productDTO = new PicturesDTO(itemRequest.name(), itemRequest.amount(), 1299L);
-                productDTOList.add(productDTO);
-            } else if (ProductName.PictureFinland.equals(itemRequest.name())) {
-                ProductDTO productDTO = new PicturesDTO(itemRequest.name(), itemRequest.amount(), 1499L);
-                productDTOList.add(productDTO);
-            } else if (ProductName.ChairElsa.equals(itemRequest.name())) {
-                ProductDTO productDTO = new ChairsDTO(itemRequest.name(), itemRequest.amount(), 500, 500, 500, "plastic");
-                productDTOList.add(productDTO);
-            } else if (ProductName.ChairKnut.equals(itemRequest.name())) {
-                ProductDTO productDTO = new ChairsDTO(itemRequest.name(), itemRequest.amount(), 400, 1000, 1500,  "wood");
-                productDTOList.add(productDTO);
-            } else if (ProductName.ChairLars.equals(itemRequest.name())) {
-                ProductDTO productDTO = new ChairsDTO(itemRequest.name(), itemRequest.amount(), 200, 2000, 3000, "metal");
-                productDTOList.add(productDTO);
-            } else if (ProductName.TableLotta.equals(itemRequest.name())) {
-                ProductDTO productDTO = new TablesDTO(itemRequest.name(), itemRequest.amount(), 1000, 1000, "plastic");
-                productDTOList.add(productDTO);
-            }else if (ProductName.TableLola.equals(itemRequest.name())) {
-                ProductDTO productDTO = new TablesDTO(itemRequest.name(), itemRequest.amount(), 3000, 1000, "plastic");
-                productDTOList.add(productDTO);
-            }
-        }
-
-        ReceiptDTO receiptDto = calculateTotalPriceUseCase.execute(new OrderDTO(productDTOList));
+        final ReceiptDTO receiptDto = calculateTotalPriceUseCase.execute(new OrderDTO(productsToOrder));
 
         return new ReceiptResponse(receiptDto.priceInCents(), receiptDto.text());
     }
