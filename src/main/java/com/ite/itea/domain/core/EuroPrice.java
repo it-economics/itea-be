1b1b4df3ab94081048f4174ce003ff531129632c
Raimund Krämer <kraemer_raimund@web.de>
2023-07-02 13:26:22 +0200

    Add tests for EuroPrice with potential exercise

--- src/main/java/com/ite/itea/domain/core/EuroPrice.java
+++ src/main/java/com/ite/itea/domain/core/EuroPrice.java
@@ -7,6 +7,14 @@ import java.util.Objects;
 
 public final class EuroPrice {
 
+    /**
+     * Note: We *could* use BigDecimal instead, and in fact we might later decide to do so.
+     * The important thing is that this is an implementation detail, and nothing externally depends
+     * on this detail. BigDecimal itself, however, is not a monetary type (although it can be used
+     * to implement one), but a numerical type. Whether negative prices should be allowed, for
+     * example, and how many decimal places we want to support (i.e., fractional cents) depends on
+     * the domain (i.e., the business rules).
+     */
     private final long cents;
 
     private EuroPrice(long cents) {
@@ -19,6 +27,16 @@ public final class EuroPrice {
 
     public static EuroPrice zero() {
         return new EuroPrice(0);
+    }
+
+    public static EuroPrice ofEurosAndCents(int euros, int cents) {
+        final var totalCents = euros * 100 + cents;
+        return new EuroPrice(totalCents);
+    }
+
+    public static EuroPrice ofEuros(int euros) {
+        final var totalCents = euros * 100;
+        return new EuroPrice(totalCents);
     }
 
     public EuroPrice plus(EuroPrice tableTopPrice) {
