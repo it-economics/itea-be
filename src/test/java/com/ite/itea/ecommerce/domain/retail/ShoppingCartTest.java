45258bb3dd04d1ff4a62b8f5544ce653102e97d8
Raimund Kraemer <kraemer_raimund@web.de>
2023-08-17 20:50:19 +0200

    Simplify tests by parameterizing
    This way we can easily add additional test cases.

--- src/test/java/com/ite/itea/ecommerce/domain/retail/ShoppingCartTest.java
+++ src/test/java/com/ite/itea/ecommerce/domain/retail/ShoppingCartTest.java
@@ -1,27 +1,41 @@
 package com.ite.itea.ecommerce.domain.retail;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShoppingCartTest {
 
-    @Test
-    void emptyCart_hasPriceOfZeroCents() {
-        final var emptyCart = ShoppingCart.empty();
-
-        assertThat(emptyCart.price()).isEqualTo(EuroPrice.ofCents(0));
+    private static Stream<ProductsAndExpectedPrice> provideShoppingCartContentsWithExpectedTotalPrice() {
+        return Stream.of(
+                new ProductsAndExpectedPrice(
+                        List.of(),
+                        EuroPrice.zero()
+                ),
+                new ProductsAndExpectedPrice(
+                        List.of(
+                                new Picture(ProductId.random(), "A beautiful picture", EuroPrice.ofCents(1337)),
+                                new Picture(ProductId.random(), "An ugly picture", EuroPrice.ofCents(42))
+                        ),
+                        EuroPrice.ofCents(1379)
+                )
+        );
     }
 
-    @Test
-    void cartWithMultipleItems_costsTheSumOfTheItemsItContains() {
+    @ParameterizedTest
+    @MethodSource("provideShoppingCartContentsWithExpectedTotalPrice")
+    void shoppingCartContentsResultInCorrectTotalPrice(ProductsAndExpectedPrice productsAndExpectedPrice) {
         final var cart = new ShoppingCart();
-        final var beautifulPicture = new Picture(ProductId.random(), "A beautiful picture", EuroPrice.ofCents(1337));
-        final var uglyPicture = new Picture(ProductId.random(), "An ugly picture", EuroPrice.ofCents(42));
 
-        cart.add(beautifulPicture);
-        cart.add(uglyPicture);
+        productsAndExpectedPrice.products.forEach(cart::add);
 
-        assertThat(cart.price()).isEqualTo(EuroPrice.ofCents(1379));
+        assertThat(cart.price()).isEqualTo(productsAndExpectedPrice.expectedTotalPrice);
+    }
+
+    private record ProductsAndExpectedPrice(Iterable<Product> products, EuroPrice expectedTotalPrice) {
     }
 }
