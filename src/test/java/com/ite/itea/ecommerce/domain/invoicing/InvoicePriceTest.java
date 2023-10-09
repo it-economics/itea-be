d542dbce94ec8101900ba103b3aed29d14b60cd1
Raimund Kraemer <rkraemer@it-economics.de>
2023-10-09 18:06:58 +0200

    Rename test class to InvoicePriceTest
    The previous name implied that it was a test for a class.
    Now it is clearer that the tests specifies some behavior,
    and is more independent from the structure of the imple-
    mentation.

--- src/test/java/com/ite/itea/ecommerce/domain/invoicing/InvoicePriceTest.java
+++ src/test/java/com/ite/itea/ecommerce/domain/invoicing/InvoicePriceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class InvoiceServiceTest {
+public class InvoicePriceTest {
 
     @Test
     void calculatesGrossPriceCorrectly() {
