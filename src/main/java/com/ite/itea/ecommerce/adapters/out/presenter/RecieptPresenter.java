c5e190ace0091fd5f6538462bc3506d9b3fa0d99
Alice <rkraemer@it-economics.de>
2024-01-18 09:27:05 +0100

    Fix order of amount and price
    The amound and price were wrong on the receipt,
    causing wrong receipt output and test failures.

--- src/main/java/com/ite/itea/ecommerce/adapters/out/presenter/RecieptPresenter.java
+++ src/main/java/com/ite/itea/ecommerce/adapters/out/presenter/RecieptPresenter.java
@@ -27,21 +27,21 @@ public class RecieptPresenter implements com.ite.itea.ecommerce.usecase.port.Rec
                 + "Total " + totalPrice(order).formatPrice();
     }
 
-    private String formatOrderItem(Order.OrderItem orderItem){
-       if(orderItem.amount() == 0){
-           return "";
-       }
+    private String formatOrderItem(Order.OrderItem orderItem) {
+        if (orderItem.amount() == 0) {
+            return "";
+        }
 
-       final var product = orderItem.product();
-       final var productName = product.name();
-       final var price = product.price().formatPrice();
-       final var amount = orderItem.amount();
-       return MessageFormat.format("{0} {1} * {2}\n", productName, amount, price);
+        final var product = orderItem.product();
+        final var productName = product.name();
+        final var price = product.price().formatPrice();
+        final var amount = orderItem.amount();
+        return MessageFormat.format("{0} {1} * {2}\n", productName, price, amount);
     }
 
     private EuroPrice totalPrice(Order order) {
-       return order.items().stream()
-               .map(orderItem -> orderItem.product().price().times(orderItem.amount()))
-               .reduce(EuroPrice.zero(), EuroPrice::plus);
+        return order.items().stream()
+                .map(orderItem -> orderItem.product().price().times(orderItem.amount()))
+                .reduce(EuroPrice.zero(), EuroPrice::plus);
     }
 }
