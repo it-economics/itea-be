2f7e1ff70520ee93cb05ae26c4e29215a2ae23ca
Raimund Kraemer <rkraemer@it-economics.de>
2024-04-15 12:01:25 +0200

    Delete obsolete banking project
    It remains on the old branch for the kata where it was used.

--- banking/src/main/java/io/jivimberg/hexagonalarchitecture/adapters/persistence/SpringDataBankAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package io.jivimberg.hexagonalarchitecture.adapters.persistence;
-
-import io.jivimberg.hexagonalarchitecture.application.domain.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringDataBankAccountRepository extends JpaRepository<BankAccount, Long> { }
