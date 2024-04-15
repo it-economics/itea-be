d690a910e385e210eb885b65f9607a8bc712f62b
Raimund Kraemer <rkraemer@it-economics.de>
2024-04-15 12:01:25 +0200

    Delete obsolete banking project
    It remains on the old branch for the kata where it was used.

--- banking/src/main/java/io/jivimberg/hexagonalarchitecture/application/port/incoming/WithdrawUseCase.java
+++
@@ -1,7 +0,0 @@
-package io.jivimberg.hexagonalarchitecture.application.port.incoming;
-
-import java.math.BigDecimal;
-
-public interface WithdrawUseCase {
-    boolean withdraw(Long id, BigDecimal amount);
-}
--- banking/src/main/java/io/jivimberg/hexagonalarchitecture/application/port/outgoing/SaveAccountPort.java
+++
@@ -1,7 +0,0 @@
-package io.jivimberg.hexagonalarchitecture.application.port.outgoing;
-
-import io.jivimberg.hexagonalarchitecture.application.domain.BankAccount;
-
-public interface SaveAccountPort {
-    void save(BankAccount bankAccount);
-}
