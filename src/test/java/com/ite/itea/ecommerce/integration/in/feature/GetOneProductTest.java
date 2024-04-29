0213f919ed87eb7d0bee928497ce4ea65daa5ab4
Danny Liebmann <dliebmann@it-economics.de>
2024-04-29 11:00:30 +0200

    adds GetOneProductTest test

---
+++ src/test/java/com/ite/itea/ecommerce/integration/in/feature/GetOneProductTest.java
@@ -0,0 +1,41 @@
+package com.ite.itea.ecommerce.integration.in.feature;
+
+
+import com.ite.itea.ecommerce.domain.core.EuroPrice;
+import com.ite.itea.ecommerce.usecase.dto.ProductResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@TestPropertySource(properties = {"management.port=0"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GetOneProductTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void shouldReturnProductSurprise() {
+        var surpriseProductId = 11L;
+        var response = this.testRestTemplate.getForEntity("http://localhost:" + this.port + "/product/" + surpriseProductId, ProductResponse.class);
+
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        then(response.getBody().product().id().internalID()).isEqualTo(11L);
+        then(response.getBody().product().name()).isEqualTo("Surprise");
+        then(response.getBody().product().imageName()).isEqualTo("surprise.png");
+        then(response.getBody().product().description()).isEqualTo("What it will be?");
+        then(response.getBody().product().price()).isEqualTo(EuroPrice.ofCents(2000));
+    }
+
+}
