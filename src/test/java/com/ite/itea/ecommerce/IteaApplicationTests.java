15ccd37c5c9d6fc9656d3aaee1713a602e6a992f
dliebmann <danny.liebmann@soprasteria.com>
2024-06-20 16:29:37 +0200

    fixes IteaApplicationTests.java

--- src/test/java/com/ite/itea/ecommerce/IteaApplicationTests.java
+++ src/test/java/com/ite/itea/ecommerce/IteaApplicationTests.java
@@ -2,8 +2,11 @@ package com.ite.itea.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IteaApplicationTests {
 
     @Test
