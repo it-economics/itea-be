52d8315a303484a7343b7bca0c96731f1552bb57
dliebmann <danny.liebmann@soprasteria.com>
2024-07-18 17:22:13 +0200

    adds an own h2database profile

--- src/test/java/com/ite/itea/ecommerce/adapters/out/persistence/product/JpaProductDatabaseRepositoryTest.java
+++ src/test/java/com/ite/itea/ecommerce/adapters/out/persistence/product/JpaProductDatabaseRepositoryTest.java
@@ -3,12 +3,14 @@ package com.ite.itea.ecommerce.adapters.out.persistence.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("h2database")
 public class JpaProductDatabaseRepositoryTest {
 
     @Autowired
--- src/test/java/com/ite/itea/ecommerce/integration/in/feature/GetOneProductTest.java
+++ src/test/java/com/ite/itea/ecommerce/integration/in/feature/GetOneProductTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 })
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
+@ActiveProfiles("h2database")
 class GetOneProductTest {
 
     @LocalServerPort
