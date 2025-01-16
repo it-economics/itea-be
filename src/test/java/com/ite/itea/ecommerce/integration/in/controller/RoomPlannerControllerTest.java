99ac666736ab934d50a00836ed5f4550f75c5783
Raimund Kraemer <33892834+kraemer-raimund@users.noreply.github.com>
2025-01-16 15:13:07 +0100

    Parameterize test

--- src/test/java/com/ite/itea/ecommerce/integration/in/controller/RoomPlannerControllerTest.java
+++ src/test/java/com/ite/itea/ecommerce/integration/in/controller/RoomPlannerControllerTest.java
@@ -5,7 +5,8 @@ import com.ite.itea.ecommerce.usecase.dto.Wall;
 import com.ite.itea.ecommerce.usecase.dto.WallPaintAmountRequest;
 import com.ite.itea.ecommerce.usecase.dto.WallPaintAmountResponse;
 import org.assertj.core.data.Offset;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,11 +32,16 @@ class RoomPlannerControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @Test
-    void determinesRequiredAmountOfWallPaintForGivenRoomAndPaint() {
+    @ParameterizedTest
+    @CsvSource(value = """
+            f9a4905d-df31-4439-bac7-96528baf5a5a,4.8,2.44,81.98
+            """)
+    void determinesRequiredAmountOfWallPaintForGivenRoomAndPaint(
+            String wallPaintId, float roomWidth, float roomHeight, float expectedRequiredAmount
+    ) {
         var wallPaintAmountRequest = new WallPaintAmountRequest(
-                new Room(List.of(new Wall(4.8f, 2.44f))),
-                "f9a4905d-df31-4439-bac7-96528baf5a5a"
+                new Room(List.of(new Wall(roomWidth, roomHeight))),
+                wallPaintId
         );
 
         var entity = this.testRestTemplate.postForEntity(
@@ -44,6 +50,6 @@ class RoomPlannerControllerTest {
                 WallPaintAmountResponse.class);
 
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(entity.getBody().requiredPaintAmountLiters()).isCloseTo(81.98f, Offset.offset(0.01f));
+        assertThat(entity.getBody().requiredPaintAmountLiters()).isCloseTo(expectedRequiredAmount, Offset.offset(0.01f));
     }
 }
