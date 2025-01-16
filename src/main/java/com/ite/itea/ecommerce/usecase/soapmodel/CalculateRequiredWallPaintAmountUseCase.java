52eda7a5af74403b41dc882a24d09154cc69a78a
Raimund Kraemer <33892834+kraemer-raimund@users.noreply.github.com>
2025-01-16 16:01:41 +0100

    Support paint amounts in gallons per square foot

--- src/main/java/com/ite/itea/ecommerce/usecase/soapmodel/CalculateRequiredWallPaintAmountUseCase.java
+++ src/main/java/com/ite/itea/ecommerce/usecase/soapmodel/CalculateRequiredWallPaintAmountUseCase.java
@@ -13,10 +13,25 @@ public class CalculateRequiredWallPaintAmountUseCase {
     }
 
     public float execute(Room room, WallPaintId wallPaintId) {
-        var paint = wallPaintRepository.byId(wallPaintId);
+        var paint = wallPaintRepository.byId(wallPaintId).get();
         var wall = room.walls().get(0);
-        var wallArea = wall.width() * wall.height();
-        var requiredPaint = paint.get().amountPerArea() * wallArea;
-        return requiredPaint;
+        var wallAreaInSquareMeters = wall.width() * wall.height();
+        switch (paint.unit()) {
+            case LITERS_PER_SQUARE_METER -> {
+                var litersPerSquareMeter = paint.amountPerArea();
+                var liters = litersPerSquareMeter * wallAreaInSquareMeters;
+                return liters;
+            }
+            case GALLONS_PER_SQAURE_FOOT -> {
+                var oneLiterInGallons = 0.2641720524f;
+                var oneSquareMeterInSquareFeet = 10.7639104f;
+                var gallonsPerSquareFoot = paint.amountPerArea();
+                var wallAreaInSquareFeet = wallAreaInSquareMeters * oneSquareMeterInSquareFeet;
+                var gallons = gallonsPerSquareFoot * wallAreaInSquareFeet;
+                var liters = gallons * (1f / oneLiterInGallons);
+                return liters;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + paint.unit());
+        }
     }
 }
--- src/test/java/com/ite/itea/ecommerce/integration/in/controller/RoomPlannerControllerTest.java
+++ src/test/java/com/ite/itea/ecommerce/integration/in/controller/RoomPlannerControllerTest.java
@@ -35,7 +35,9 @@ class RoomPlannerControllerTest {
     @ParameterizedTest
     @CsvSource(value = {
             "f9a4905d-df31-4439-bac7-96528baf5a5a,4.8,2.44,81.98",
-            "c9cf727e-f676-4d9c-b771-2df6a5e8fc51,13.37,3.14,419.81"
+            "c9cf727e-f676-4d9c-b771-2df6a5e8fc51,13.37,3.14,419.81",
+            "a35e6a36-e0a9-4a41-b747-76df053f6669,13.37,3.14,419.81",
+            "9d8da8d1-29e2-4fbd-8841-d43c57df18d1,4.8,2.44,81.98",
     })
     void determinesRequiredAmountOfWallPaintForGivenRoomAndPaint(
             String wallPaintId, float roomWidth, float roomHeight, float expectedRequiredAmount
@@ -51,6 +53,6 @@ class RoomPlannerControllerTest {
                 WallPaintAmountResponse.class);
 
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(entity.getBody().requiredPaintAmountLiters()).isCloseTo(expectedRequiredAmount, Offset.offset(0.01f));
+        assertThat(entity.getBody().requiredPaintAmountLiters()).isCloseTo(expectedRequiredAmount, Offset.offset(0.1f));
     }
 }
