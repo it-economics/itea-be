26042c9fd8dc2c628d4c9152aaa98909474fa0f9
KenzaIdrissi <kenza.idrissi@soprassteria.com>
2025-03-20 13:31:56 +0100

    rerun

--- provider.tf
+++ provider.tf
@@ -3,12 +3,12 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      version = "~> 2.21.0"
 }
 }
-# #supports state locking remotly. When locking support is enabled it will use LOCK and UNLOCK requests providing the lock info in the body
-#   backend "http" {
-#   }
+#supports state locking remotly. When locking support is enabled it will use LOCK and UNLOCK requests providing the lock info in the body
+  # backend "http" {
+  # }
 }
 ## A provider is a plugin that Terraform uses to create and manage your resources.
 provider "docker" {}
