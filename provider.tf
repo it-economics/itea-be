12022b4bf286c60561feaa76bc238b87a584757e
KenzaIdrissi <kenza.idrissi@soprassteria.com>
2025-03-20 14:05:52 +0100

    tst

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@
 #supports state locking remotly. When locking support is enabled it will use LOCK and UNLOCK requests providing the lock info in the body
   # backend "http" {
   # }
-}
+#}
 ## A provider is a plugin that Terraform uses to create and manage your resources.
 provider "docker" {
     host = "unix:///var/run/docker.sock"
